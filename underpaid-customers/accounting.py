36ed0d39d84f1a62edf611b593a5dfadd59223d5
ameliacgreen <ameliacgreen@gmail.com>
2017-02-23 04:30:16 +0000

    Created function for finding underpaying customers

--- underpaid-customers/accounting.py
+++ underpaid-customers/accounting.py
@@ -1,28 +1,21 @@
 melon_cost = 1.00
 
-customer1_name = "Joe"
-customer1_melons = 5
-customer1_paid = 5.00
+def read_customer_info(file, melon_cost):
+    new_file = open(file)
+    for line in new_file:
+        line = line.rstrip()
+        words = line.split('|')
 
-customer2_name = "Frank"
-customer2_melons = 6
-customer2_paid = 6.00
+        customer_name = words[1]
+        customer_melons = int(words[2])
+        customer_paid = float(words[3])
+        customer_expected = (customer_melons * melon_cost)
+        if customer_expected > customer_paid:
+            print customer_name, "paid {}, expected {}".format(customer_paid, customer_expected)
 
-customer3_name = "Sally"
-customer3_melons = 3
-customer3_paid = 3.00
+read_customer_info("customer-orders.txt", melon_cost)
 
-customer4_name = "Sean"
-customer4_melons = 9
-customer4_paid = 9.50
-
-customer5_name = "David"
-customer5_melons = 4
-customer5_paid = 4.00
-
-customer6_name = "Ashley"
-customer6_melons = 3
-customer6_paid = 2.00
+"""
 
 customer1_expected = customer1_melons * melon_cost
 if customer1_expected != customer1_paid:
@@ -53,3 +46,4 @@ customer6_expected = customer6_melons * melon_cost
 if customer6_expected != customer6_paid:
     print customer6_name, "paid {:.2f}, expected {:.2f}".format(
         customer6_paid, customer6_expected)
+"""
